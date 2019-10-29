505b061de8cf29e7de726df2bb8cc6fccb796a3f
lock <lockexit@qq.com>
2019-10-29 10:54:36 +0800

    gochat

--- api/handler/push.go
+++ api/handler/push.go
@@ -66,7 +66,6 @@ func Push(c *gin.Context) {
 type FormRoom struct {
 	AuthToken string `form:"authToken" json:"authToken" binding:"required"`
 	Msg       string `form:"msg" json:"msg" binding:"required"`
-	ToUserId  string `form:"toUserId" json:"toUserId" binding:"required"`
 	RoomId    int    `form:"roomId" json:"roomId" binding:"required"`
 }
 
