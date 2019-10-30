ec80d6478459677dfdacb3a3c8823be45aa973a6
lock <lockexit@qq.com>
2019-10-30 23:14:13 +0800

    gochat

--- site/js/chat.js
+++ site/js/chat.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
             }
         },
         error: function () {
-            swal("sorry, exception！");
+            swal("sorry, exception!");
         }
     });
 
@@ -40,7 +40,7 @@ $(document).ready(function () {
         let data = JSON.parse(evt.data);
         if (data.op == 3) {
             // send msg to room
-            let innerInfo = '<div class="item" ><p class="nick guest j-nick " data-role="guest" data-account="">' + data.fromUserName + '(' + data.createTime + ')</p><p class="text">' + data.msg + '</p></div>';
+            let innerInfo = '<div class="item" ><p class="nick guest j-nick " data-role="guest" data-account="">' + data.fromUserName + ' (' + data.createTime + ')</p><p class="text">' + data.msg + '</p></div>';
             msg.innerHTML += innerInfo + '<br>';
         } else if (data.op == 4) {
             // get room user count
@@ -67,11 +67,11 @@ function getRoomInfo() {
         data: JSON.stringify(jsonData),
         success: function (result) {
             if (result.code != 0) {
-                swal("request error，please try again later！");
+                //swal("request error，please try again later！");
             }
         },
         error: function () {
-            swal("sorry, exception！");
+            swal("sorry, exception!");
         }
     });
 }
@@ -86,11 +86,11 @@ function getRoomUserCount() {
         data: JSON.stringify(jsonData),
         success: function (result) {
             if (result.code != 0) {
-                swal("request error，please try again later！");
+                swal("request error，please try again later!");
             }
         },
         error: function () {
-            swal("sorry, exception！");
+            swal("sorry, exception!");
         }
     });
 }
