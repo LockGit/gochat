dffddbf61b1f600be9a7b6b9c24d4c8928f00ad2
jiangbiao <154238669@qq.com>
2021-06-29 15:08:29 +0800

    恢复文件

--- main.go
+++ main.go
@@ -8,7 +8,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"gochat/api"
+	"gochat/connect"
+	"gochat/logic"
 	"gochat/site"
+	"gochat/task"
 
 	//"gochat/task"
 	"os"
@@ -21,23 +25,23 @@ func main() {
 	flag.StringVar(&module, "module", "", "assign run module")
 	flag.Parse()
 	fmt.Println(fmt.Sprintf("start run %s module", module))
-	//switch module {
-	//case "logic":
-	//	logic.New().Run()
-	//case "connect_websocket":
-	//	connect.New().Run()
-	//case "connect_tcp":
-	//	connect.New().RunTcp()
-	//case "task":
-	//	task.New().Run()
-	//case "api":
-	//	api.New().Run()
-	//case "site":
+	switch module {
+	case "logic":
+		logic.New().Run()
+	case "connect_websocket":
+		connect.New().Run()
+	case "connect_tcp":
+		connect.New().RunTcp()
+	case "task":
+		task.New().Run()
+	case "api":
+		api.New().Run()
+	case "site":
 		site.New().Run()
-	//default:
-	//	fmt.Println("exiting,module param error!")
-	//	return
-	//}
+	default:
+		fmt.Println("exiting,module param error!")
+		return
+	}
 	fmt.Println(fmt.Sprintf("run %s module done!", module))
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
