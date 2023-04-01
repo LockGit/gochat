489819d62ee54ed35204b6ba83e2573f5960232f
lock <lockexit@gmail.com>
2023-04-01 15:10:53 +0800

    ❤️ default ip

--- reload.sh
+++ reload.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "replace api and websocket ip address..."
-addr_http=${HOST_IP}:7070
-addr_ws=${HOST_IP}:7000
+addr_http=${HOST_IP-127.0.0.1}:7070
+addr_ws=${HOST_IP-127.0.0.1}:7000
 
 sed -r -i "s/\/\/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b:[0-9]+/\/\/${addr_http}/g" site/static/js/main.06044d49.js
 sed -r -i "s/\/\/(\b[0-9]{1,3}\.){3}[0-9]{1,3}\b:[0-9]+\/ws/\/\/${addr_ws}\/ws/g" site/static/js/main.06044d49.js
